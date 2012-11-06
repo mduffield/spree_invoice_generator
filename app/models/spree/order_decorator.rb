46dc0d2ab67b3c42a9f5a78ae28cd4fbd4839dfc
Damiano <damiano.giacomello@diginess.it>
2012-11-06 18:02:25 +0100

    fixed error for ruby 1.8.7

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,6 +6,6 @@ Spree::Order.class_eval do
   def add_invoice
   	# Only create an invoice if the order is completed!
   	# And only create it if there is no invoice yet.
-    self.create_invoice(user: user) if self.completed? && self.invoice.blank?
+    self.create_invoice(:user => user) if self.completed? && self.invoice.blank?
   end
 end
